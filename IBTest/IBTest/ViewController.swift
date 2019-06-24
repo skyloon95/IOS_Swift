a8462d7f60b4a68301bc8f07fb82547dc8bf157b
모설아 <skyloon95@gmail.com>
2019-06-24 11:03:13 +0900

    cocoa 프레임워크 관련 정리와 테스트

---
+++ IBTest/IBTest/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  IBTest
+//
+//  Created by 모설아 on 23/06/2019.
+//  Copyright © 2019 Snow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet var uiTitle01: UILabel!
+    @IBOutlet var uiTitle02: UILabel!
+    @IBOutlet var uiTitle03: UILabel!
+    @IBOutlet var uiTitle04: UILabel!
+    @IBOutlet var uiTitle05: UILabel!
+    
+    
+    @IBAction func clickBtn01(_ sender: Any) {
+        self.uiTitle01.text = "Button01 Clicked"
+    }
+    
+    @IBAction func clickBtn02(_ sender: Any) {
+        self.uiTitle02.text = "Button02 Clicked"
+    }
+    
+    @IBAction func clickBtn03(_ sender: Any) {
+        self.uiTitle03.text = "Button03 Clicked"
+    }
+    
+    @IBAction func clickBtn04(_ sender: Any) {
+        self.uiTitle04.text = "Button04 Clicked"
+    }
+    
+    @IBAction func clickBtn05(_ sender: Any) {
+        self.uiTitle05.text = "Button05 Clicked"
+    }
+}
+
